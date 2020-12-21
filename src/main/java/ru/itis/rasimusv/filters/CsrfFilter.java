b1e4f741a6809b62b49f2309a03a6f1bcc88b610
rasimusv <rasimusv@gmail.com>
2020-12-21 23:58:57 +0300

    CSRF implemented

---
+++ src/main/java/ru/itis/rasimusv/filters/CsrfFilter.java
@@ -0,0 +1,44 @@
+package ru.itis.rasimusv.filters;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+public class CsrfFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        if (request.getMethod().equals("GET")) {
+            String csrf = UUID.randomUUID().toString();
+            request.setAttribute("_csrf_token", csrf);
+            request.getSession().setAttribute("_csrf_token", csrf);
+        }
+
+        if (request.getMethod().equals("POST")) {
+            String requestCsrf = request.getParameter("_csrf_token");
+            String sessionCsrf = (String) request.getSession(false).getAttribute("_csrf_token");
+            if (requestCsrf.equals(sessionCsrf)) {
+                filterChain.doFilter(servletRequest, servletResponse);
+                return;
+            } else {
+                response.sendRedirect("/login");
+                return;
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
